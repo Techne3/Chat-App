14359f7d3eeede3cce1653fdb481cc9701b69335
Timothy Sharkey <Techne3@gmail.com>
2020-02-09 13:31:52 -0700

    added stying to both chat and join components

---
+++ client/src/components/Chat/Chat.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from "react";
+import queryString from "query-string";
+import io from "so";
+
+function Chat() {
+  return <h1>Chat</h1>;
+}
+
+export default Chat;
--- server/index.js
+++ server/index.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const socketio = require("socket.io");
 const http = require("http");
+const router = require("./router");
 
 const PORT = process.env.PORT || 5500;
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+
+// socket setup
+io.on("connection", socket => {
+  console.log("we have a new connection!!");
+
+  socket.on("disconnect", () => {
+    console.log("user has left!!");
+  });
+});
+
+app.use(router);
 
 server.listen(PORT, () => console.log(`Sever has started on ${PORT}`));
